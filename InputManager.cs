21095915c7105a581442ec20be248dd7d8832c10
Glenn <glennwiz@gmail.com>
2023-10-24 23:03:27 +0200

    Refactor InputManager to improve readability
    Refactored InputManager class in GameOfLife to make it more readable by breaking up large function into smaller manageable ones. This resulted in more compact, self-descriptive methods, therefore making the code easier to understand and maintain. Leveraged helper methods to improve code organization and efficiency. Changes were also made to handle key and mouse events in a cleaner way, as well as in the methods that manage the grid.

--- InputManager.cs
+++ InputManager.cs
@@ -1,123 +1,150 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Godot;
 
-namespace GameOfLife;
-
-public partial class InputManager : Node2D
+namespace GameOfLife
 {
-	private readonly Grid _grid;
+    public partial class InputManager : Node2D
+    {
+        private readonly Grid _grid;
 
-	public InputManager(Grid grid)
-	{
-		_grid = grid;
-	}
+        public InputManager(Grid grid)
+        {
+            _grid = grid;
+        }
 
-	[SuppressMessage("ReSharper", "ConditionIsAlwaysTrueOrFalse")]
-	public override void _Input(InputEvent @event)
-	{
-		if (Input.IsKeyPressed(Key.Space))
-		{
-			_grid.IsPaused = !_grid.IsPaused;
-		}
-		
-		if (_grid.IsPaused) {
-			if (Input.IsKeyPressed(Key.Left)) {
-				_grid.Rewind();
-			}
-			if (Input.IsKeyPressed(Key.Right)) {
-				_grid.StepForward();
-			}
-		}
+        [SuppressMessage("ReSharper", "ConditionIsAlwaysTrueOrFalse")]
+        public override void _Input(InputEvent @event)
+        {
+            HandleKeyPressEvents();
+            HandleMouseInputEvents(@event);
+        }
 
-		// Handle 'S' key press to clear cells and reset speed
-		if (Input.IsKeyPressed(Key.S))
-		{
-			_grid.MatrixManipulation.ClearGrid(_grid);
-			_grid.ResetUpdateTickRate();
-			
-			//we need to clear the whole List of grids
-			
+        private void HandleKeyPressEvents()
+        {
+            if (Input.IsKeyPressed(Key.Space))
+            {
+                TogglePause();
+            }
 
-			QueueRedraw();
-		}
+            if (_grid.IsPaused)
+            {
+                HandleRewindStepForward();
+            }
+                
+            if (Input.IsKeyPressed(Key.S))
+            {
+                HandleGridReset();
+            }
 
-		if (Input.IsKeyPressed(Key.Q))
-		{
-			_grid.DrawDeadCell = !_grid.DrawDeadCell;
-		}
+            if (Input.IsKeyPressed(Key.Q))
+            {
+                ToggleDrawDeadCell();
+            }
 
-		if (Input.IsKeyPressed(Key.Up))
-		{
-			_grid.UpdateTickRate -= 0.1f;
-		}
+            if (Input.IsKeyPressed(Key.Up))
+            {
+                DecreaseTickRate();
+            }
 
-		if (Input.IsKeyPressed(Key.Down))
-		{
-			_grid.UpdateTickRate += 0.1f;
-		}
+            if (Input.IsKeyPressed(Key.Down))
+            {
+                IncreaseTickRate();
+            }
 
-		// Handle mouse input to toggle cells
-		if (@event is InputEventMouseButton mouseButtonEvent)
-		{
-			if (mouseButtonEvent.DoubleClick)
-			{
-				_grid.IsMouseDown = false;
-				 QueueRedraw();
-				return;
-			}
+            if (Input.IsKeyPressed(Key.R))
+            {
+                HandleMirrorAndShift();
+            }
 
-			if (mouseButtonEvent.Pressed)
-			{
-				_grid.IsMouseDown = mouseButtonEvent.Pressed;
-				if (_grid.IsMouseDown)
-				{
-					var mousePosition = mouseButtonEvent.Position;
-					var x = (int) (mousePosition.X / _grid.BoxSize);
-					var y = (int) (mousePosition.Y / _grid.BoxSize);
-					_grid.ToggleCell(new Vector2(x, y));
-					QueueRedraw();
-				}
+            if (Input.IsKeyPressed(Key.Key1))
+            {
+                DrawGliderPattern();
+            }
 
-				if (!_grid.IsMouseDown)
-				{
-					_grid.IsMouseDown = false;
-					QueueRedraw();
-				}
-			}
-		}
+            if (Input.IsKeyPressed(Key.Key2))
+            {
+                DrawGosperGliderGunPattern();
+            }
 
-		if (Input.IsKeyPressed(Key.R))
-		{
-    		_grid.MirrorAndShift();
-		}
-		
-		if (Input.IsKeyPressed(Key.Key1))
-		{
-			// create the walker pattern at mouse position
-			var gliderCells = new[,]
-			{
-				{false, true , false},
-				{false, false, true},
-				{true , true , true}
-			};
+            if (Input.IsKeyPressed(Key.Right))
+            {
+                GD.Print("Right");
+            }
+        }
 
-			var glider = new PatternCreator.Pattern(3, 3, gliderCells);
-			_grid.DrawPattern(glider);
-		}
+        private void HandleMouseInputEvents(InputEvent @event)
+        {
+            if (@event is InputEventMouseButton mouseButtonEvent)
+            {
+                HandleMouseClick(mouseButtonEvent);
+            }
+        }
 
-		if (Input.IsKeyPressed(Key.Key2))
-		{
-			const bool f = false; //too easier read the pattern in the grid
-		
-			var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.GosperGun);
-			
-			var gosperGliderGun = new PatternCreator.Pattern(36, 9, rotated);
-			_grid.DrawPattern(gosperGliderGun);
-		}
+        private void HandleMouseClick(InputEventMouseButton mouseButtonEvent)
+        {
+            if (mouseButtonEvent.DoubleClick && !mouseButtonEvent.Pressed)
+            {
+                _grid.IsMouseDown = false;
+                QueueRedraw();
+                return;
+            }
 
-		if (Input.IsKeyPressed(Key.Right))
-		{
-			GD.Print("Right");
-		}
-	}
+            if (mouseButtonEvent.Pressed)
+            {
+                _grid.IsMouseDown = mouseButtonEvent.Pressed;
+                if (_grid.IsMouseDown)
+                {
+                    ToggleCell(mouseButtonEvent.Position);
+                }
+            }
+        }
+
+        private void TogglePause() => _grid.IsPaused = !_grid.IsPaused;
+        private void HandleRewindStepForward()
+        {
+            if (Input.IsKeyPressed(Key.Left)) _grid.Rewind();
+            if (Input.IsKeyPressed(Key.Right)) _grid.StepForward();
+        }
+
+        private void HandleGridReset()
+        {
+            _grid.MatrixManipulation.ClearGrid(_grid);
+            _grid.ResetUpdateTickRate();
+            QueueRedraw();
+        }
+
+        private void ToggleDrawDeadCell() => _grid.DrawDeadCell = !_grid.DrawDeadCell;
+        private void DecreaseTickRate() => _grid.UpdateTickRate -= 0.1f;
+        private void IncreaseTickRate() => _grid.UpdateTickRate += 0.1f;
+        private void HandleMirrorAndShift() => _grid.MirrorAndShift();
+
+        private void ToggleCell(Vector2 position)
+        {
+            var x = (int)(position.X / _grid.BoxSize);
+            var y = (int)(position.Y / _grid.BoxSize);
+            _grid.ToggleCell(new Vector2(x, y));
+            QueueRedraw();
+        }
+
+        private void DrawGliderPattern()
+        {
+            var gliderCells = new[,]
+            {
+                {false, true , false},
+                {false, false, true},
+                {true , true , true}
+            };
+
+            var glider = new PatternCreator.Pattern(3, 3, gliderCells);
+            _grid.DrawPattern(glider);
+        }
+
+        private void DrawGosperGliderGunPattern()
+        {
+            const bool f = false;
+            var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.GosperGun);
+            var gosperGliderGun = new PatternCreator.Pattern(36, 9, rotated);
+            _grid.DrawPattern(gosperGliderGun);
+        }
+    }
 }
