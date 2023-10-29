8fbec8aafb92c89708b8127f026dc7ebe6b866e1
Glenn <glennwiz@gmail.com>
2023-10-29 15:05:52 +0100

    Refactored code to comply with C# 10 syntax
    In this commit, we have updated the syntax of two files, 'Cell.cs' and 'InputManager.cs', to align with the changes in C# 10. The changes are mainly about the new syntax for namespaces that allows for a more compact hierarchy and the removal of unnecessary using statements. Specifically, we've brought in the semicolon-separated, multiline namespaces syntax into the codebase to enhance readability and ease of structuring. We've also removed a redundant `using System.Diagnostics.CodeAnalysis;` line since it was unused. This refactoring is aimed to make our code more maintainable and up-to-date with modern C# practices.

--- Cell.cs
+++ Cell.cs
@@ -1,5 +1,7 @@
 ﻿using Godot;
 
+namespace GameOfLife;
+
 public class Cell
 {
     public bool IsAlive { get; set; }
--- InputManager.cs
+++ InputManager.cs
@@ -1,220 +1,218 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Godot;
+﻿using Godot;
 
-namespace GameOfLife 
+namespace GameOfLife;
+
+public partial class InputManager : Node2D 
 {
-    public partial class InputManager : Node2D 
+    private readonly Grid _grid;
+    private void TogglePause() => _grid.IsPaused = !_grid.IsPaused;
+
+    public InputManager(Grid grid) 
     {
-        private readonly Grid _grid;
-        private void TogglePause() => _grid.IsPaused = !_grid.IsPaused;
-
-        public InputManager(Grid grid) 
-        {
-            _grid = grid;
-        }
+        _grid = grid;
+    }
         
-        public override void _Input(InputEvent @event) 
-        {
-            HandleKeyPressEvents();
-            HandleMouseInputEvents(@event);
-        }
+    public override void _Input(InputEvent @event) 
+    {
+        HandleKeyPressEvents();
+        HandleMouseInputEvents(@event);
+    }
 
-        private void HandleKeyPressEvents()
-        {
-            HandleSpaceKeyPress();
-            HandlePauseDependentKeyPresses();
-            HandleSKeyPress();
-            HandleQKeyPress();
-            HandleUpKeyPress();
-            HandleDownKeyPress();
-            HandleTKeyPress();
-            HandleNumberKeyPresses();
-            HandleRKeyPress();
-        }
+    private void HandleKeyPressEvents()
+    {
+        HandleSpaceKeyPress();
+        HandlePauseDependentKeyPresses();
+        HandleSKeyPress();
+        HandleQKeyPress();
+        HandleUpKeyPress();
+        HandleDownKeyPress();
+        HandleTKeyPress();
+        HandleNumberKeyPresses();
+        HandleRKeyPress();
+    }
 
-        private void HandleNumberKeyPresses()
-        {
-            if (Input.IsKeyPressed(Key.Key1)) DrawGliderPattern();
-            if (Input.IsKeyPressed(Key.Key2)) DrawGosperGliderGunPattern();
-            if (Input.IsKeyPressed(Key.Key3)) DrawPulsarPattern();
-            if (Input.IsKeyPressed(Key.Key4)) DrawOwlPattern();
-        }
+    private void HandleNumberKeyPresses()
+    {
+        if (Input.IsKeyPressed(Key.Key1)) DrawGliderPattern();
+        if (Input.IsKeyPressed(Key.Key2)) DrawGosperGliderGunPattern();
+        if (Input.IsKeyPressed(Key.Key3)) DrawPulsarPattern();
+        if (Input.IsKeyPressed(Key.Key4)) DrawOwlPattern();
+    }
         
-        private void HandleSpaceKeyPress()
+    private void HandleSpaceKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.Space)) 
         {
-            if (Input.IsKeyPressed(Key.Space)) 
-            {
-                ResetHigherGridArray();
-                TogglePause();
-            }
+            ResetHigherGridArray();
+            TogglePause();
         }
+    }
 
        
         
-        private void HandlePauseDependentKeyPresses()
-        {
-            if (!_grid.IsPaused) return;
-            HandleKeyPressedLeftRight();
-        }
+    private void HandlePauseDependentKeyPresses()
+    {
+        if (!_grid.IsPaused) return;
+        HandleKeyPressedLeftRight();
+    }
 
-        private void HandleKeyPressedLeftRight()
-        {
-            if (Input.IsKeyPressed(Key.Left)) _grid.Rewind();
-            if (Input.IsKeyPressed(Key.Right)) _grid.StepForward();
-        }
+    private void HandleKeyPressedLeftRight()
+    {
+        if (Input.IsKeyPressed(Key.Left)) _grid.Rewind();
+        if (Input.IsKeyPressed(Key.Right)) _grid.StepForward();
+    }
                 
-        private void HandleSKeyPress()
+    private void HandleSKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.S)) 
         {
-            if (Input.IsKeyPressed(Key.S)) 
-            {
-                HandleGridReset();
-            }
+            HandleGridReset();
         }
+    }
 
-        private void HandleQKeyPress()
+    private void HandleQKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.Q))
         {
-            if (Input.IsKeyPressed(Key.Q))
-            {
-                ToggleDrawDeadCell();
-            }
+            ToggleDrawDeadCell();
         }
+    }
 
-        private void HandleUpKeyPress()
+    private void HandleUpKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.Up)) 
         {
-            if (Input.IsKeyPressed(Key.Up)) 
-            {
-                HideRichTextLabel();
-                DecreaseTickRate();
-            }
+            HideRichTextLabel();
+            DecreaseTickRate();
         }
+    }
         
-        private void HandleDownKeyPress()
+    private void HandleDownKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.Down))
         {
-            if (Input.IsKeyPressed(Key.Down))
-            {
-                IncreaseTickRate();
-            }
+            IncreaseTickRate();
         }
+    }
 
-        private void HandleTKeyPress()
+    private void HandleTKeyPress()
+    {
+        if (Input.IsKeyPressed(Key.T))
         {
-            if (Input.IsKeyPressed(Key.T))
-            {
-                HandleMirrorAndShift();
-            }
+            HandleMirrorAndShift();
         }
+    }
 
-        private void HandleMouseClicked()
-        {
-            var randomPattern = PatternCreator.CreateRandomPattern(30, 30, 0.1f);
-            _grid.DrawPattern(randomPattern);
-        }
+    private void HandleMouseClicked()
+    {
+        var randomPattern = PatternCreator.CreateRandomPattern(30, 30, 0.1f);
+        _grid.DrawPattern(randomPattern);
+    }
         
-        private void DrawOwlPattern()
-        {
-            var pattern = new PatternCreator.Pattern(PatternCreator.Pattern.CustomPattern);
-            _grid.DrawPattern(pattern);
-        }
+    private void DrawOwlPattern()
+    {
+        var pattern = new PatternCreator.Pattern(PatternCreator.Pattern.CustomPattern);
+        _grid.DrawPattern(pattern);
+    }
 
-        private void HandleRKeyPress()
-        {
-            if (!Input.IsKeyPressed(Key.R)) return;
+    private void HandleRKeyPress()
+    {
+        if (!Input.IsKeyPressed(Key.R)) return;
             
-            var randomPattern = PatternCreator.CreateRandomPattern(30, 30, 0.1f);
-            _grid.DrawPattern(randomPattern);
-        }
+        var randomPattern = PatternCreator.CreateRandomPattern(30, 30, 0.1f);
+        _grid.DrawPattern(randomPattern);
+    }
 
-        private void HandleMouseInputEvents(InputEvent @event)
-        {
-            if (!(@event is InputEventMouseButton mouseButtonEvent)) return;
-            HandleMouseClick(mouseButtonEvent);
-        }
+    private void HandleMouseInputEvents(InputEvent @event)
+    {
+        if (!(@event is InputEventMouseButton mouseButtonEvent)) return;
+        HandleMouseClick(mouseButtonEvent);
+    }
 
-        private void HandleMouseClick(InputEventMouseButton mouseButtonEvent)
-        {
-            ProcessMousePress(mouseButtonEvent);
-            ProcessMouseRelease(mouseButtonEvent);
-        }
+    private void HandleMouseClick(InputEventMouseButton mouseButtonEvent)
+    {
+        ProcessMousePress(mouseButtonEvent);
+        ProcessMouseRelease(mouseButtonEvent);
+    }
 
-        private void ProcessMouseRelease(InputEventMouseButton mouseButtonEvent)
+    private void ProcessMouseRelease(InputEventMouseButton mouseButtonEvent)
+    {
+        if (mouseButtonEvent.DoubleClick) 
         {
-            if (mouseButtonEvent.DoubleClick) 
-            {
-                GD.Print("!Double click!");
-                _grid.IsMouseDown = false;
-                QueueRedraw();
-            }
-        }
-
-        private void ProcessMousePress(InputEventMouseButton mouseButtonEvent)
-        {
-            if (!mouseButtonEvent.Pressed) return;
-            
-            _grid.IsMouseDown = true;
-            ToggleCell(mouseButtonEvent.Position);
-        }
-
-        private void HandleGridReset()
-        {
-            _grid.MatrixManipulation.ClearGrid(_grid);
-            _grid.ResetUpdateTickRate();
+            GD.Print("!Double click!");
+            _grid.IsMouseDown = false;
             QueueRedraw();
         }
+    }
 
-        private void ToggleDrawDeadCell() => _grid.DrawDeadCell = !_grid.DrawDeadCell;
-        private void DecreaseTickRate() => _grid.UpdateTickRate -= 0.1f;
-        private void IncreaseTickRate() => _grid.UpdateTickRate += 0.1f;
-        private void HandleMirrorAndShift() => _grid.MirrorAndShift();
-
-        private void ToggleCell(Vector2 position)
-        {
-            var x = (int)(position.X / _grid.BoxSize);
-            var y = (int)(position.Y / _grid.BoxSize);
-            _grid.ToggleCell(new Vector2(x, y));
-            QueueRedraw();
-        }
-
-        private void DrawGliderPattern()
-        {
-            var gliderCells = new[,]
-            {
-                {false, true , false},
-                {false, false, true},
-                {true , true , true}
-            };
-
-            var glider = new PatternCreator.Pattern(gliderCells);
-            _grid.DrawPattern(glider);
-        }
-
-        private void DrawGosperGliderGunPattern()
-        {
-            var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.GosperGun);
-            var gosperGliderGun = new PatternCreator.Pattern(rotated);
-            _grid.DrawPattern(gosperGliderGun);
-        }
-        
-        private void DrawPulsarPattern()
-        {
-            var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.Pulsar);
-            var pulsarPattern = new PatternCreator.Pattern(rotated);
-            _grid.DrawPattern(pulsarPattern);
-        }
-        
-        private void HideRichTextLabel()
-        {
-            NodePath path = "../RichTextLabel";
-            var childNode = GetNode<RichTextLabel>(path);
-            childNode.Visible = false;
-            GD.Print(childNode.Name);
-        }
-                
-        private void ResetHigherGridArray()
-        {
-            if (!_grid.ResetHigherGridArray) return;
+    private void ProcessMousePress(InputEventMouseButton mouseButtonEvent)
+    {
+        if (!mouseButtonEvent.Pressed) return;
             
-            var currentStateIndex = _grid.CurrentStateIndex;
-            _grid.ListOfCellArrayStates.RemoveRange(currentStateIndex + 1, _grid.ListOfCellArrayStates.Count - currentStateIndex - 1);
-        }
+        _grid.IsMouseDown = true;
+        ToggleCell(mouseButtonEvent.Position);
+    }
+
+    private void HandleGridReset()
+    {
+        _grid.MatrixManipulation.ClearGrid(_grid);
+        _grid.ResetUpdateTickRate();
+        QueueRedraw();
+    }
+
+    private void ToggleDrawDeadCell() => _grid.DrawDeadCell = !_grid.DrawDeadCell;
+    private void DecreaseTickRate() => _grid.UpdateTickRate -= 0.1f;
+    private void IncreaseTickRate() => _grid.UpdateTickRate += 0.1f;
+    private void HandleMirrorAndShift() => _grid.MirrorAndShift();
+
+    private void ToggleCell(Vector2 position)
+    {
+        var x = (int)(position.X / _grid.BoxSize);
+        var y = (int)(position.Y / _grid.BoxSize);
+        _grid.ToggleCell(new Vector2(x, y));
+        QueueRedraw();
+    }
+
+    private void DrawGliderPattern()
+    {
+        var gliderCells = new[,]
+        {
+            {false, true , false},
+            {false, false, true},
+            {true , true , true}
+        };
+
+        var glider = new PatternCreator.Pattern(gliderCells);
+        _grid.DrawPattern(glider);
+    }
+
+    private void DrawGosperGliderGunPattern()
+    {
+        var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.GosperGun);
+        var gosperGliderGun = new PatternCreator.Pattern(rotated);
+        _grid.DrawPattern(gosperGliderGun);
+    }
+        
+    private void DrawPulsarPattern()
+    {
+        var rotated = MatrixManipulation.RotateMatrix90(PatternCreator.Pattern.Pulsar);
+        var pulsarPattern = new PatternCreator.Pattern(rotated);
+        _grid.DrawPattern(pulsarPattern);
+    }
+        
+    private void HideRichTextLabel()
+    {
+        NodePath path = "../RichTextLabel";
+        var childNode = GetNode<RichTextLabel>(path);
+        childNode.Visible = false;
+        GD.Print(childNode.Name);
+    }
+                
+    private void ResetHigherGridArray()
+    {
+        if (!_grid.ResetHigherGridArray) return;
+            
+        var currentStateIndex = _grid.CurrentStateIndex;
+        _grid.ListOfCellArrayStates.RemoveRange(currentStateIndex + 1, _grid.ListOfCellArrayStates.Count - currentStateIndex - 1);
     }
 }
