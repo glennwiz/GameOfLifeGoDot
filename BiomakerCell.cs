f3f97dca2efd451d6c2633056d04e1f4f9f7584a
Glenn <glennwiz@gmail.com>
2023-11-14 20:53:21 +0100

    added the starter biomakerCA cell

---
+++ BiomakerCell.cs
@@ -0,0 +1,63 @@
+public enum CellType
+{
+    Void,
+    Air,
+    Earth,
+    Immovable,
+    Sun,
+    OutOfBounds,
+    AgentUnspecialized,
+    AgentRoot,
+    AgentLeaf,
+    AgentFlower
+    // ... other cell types
+}
+
+public interface INutrientConsumer
+{
+    void AbsorbNutrients();
+}
+
+public class Cell
+{
+    public CellType Type { get; private set; }
+    public float StructuralIntegrity { get; private set; }
+    public int Age { get; private set; }
+    public float EarthNutrients { get; private set; }
+    public float AirNutrients { get; private set; }
+    public string AgentId { get; private set; }
+    // ... other properties as needed
+
+    public Cell(CellType type)
+    {
+        Type = type;
+        // Initialize other properties as needed
+    }
+
+    // Methods for aging, structural integrity adjustment, etc.
+
+    public void SetAgentId(string id)
+    {
+        // Validation logic before setting the AgentId
+        AgentId = id;
+    }
+
+    // ... other methods as needed
+}
+
+public class AgentCell : Cell, INutrientConsumer
+{
+    public AgentCell(CellType type) : base(type)
+    {
+        // Additional initialization for agent cells
+    }
+
+    public void AbsorbNutrients()
+    {
+        // Implementation for absorbing nutrients
+    }
+
+    // ... other agent-specific behaviors
+}
+
+// ... additional subclasses for specific agent types if needed
